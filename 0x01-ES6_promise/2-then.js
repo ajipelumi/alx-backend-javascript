22e6c135aa3ecf7f09cb91fc1f7eac90b3ec372d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-06-28 19:05:03 +0100

    implemented then and catch methods

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  })).catch(() => Error()).finally(() => {
-    console.log('Got a response from the API');
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
