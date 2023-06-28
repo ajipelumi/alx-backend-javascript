e17a33c7a2d4ddc19bd62df01cd5c63670e23d2c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-06-28 20:47:50 +0100

    handled multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+    .then((values) => {
+      for (const value of values) {
+        if (value.status === 'rejected') {
+          value.value = `Error: ${value.reason.message}`;
+          delete value.reason;
+        }
+      }
+      return values;
+    });
 }
