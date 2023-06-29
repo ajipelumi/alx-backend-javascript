7a9391d502476758a9431261fec69bf7757baedc
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-06-29 17:27:36 +0100

    added pricing functionality

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,52 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency');
+    }
+
+    /* eslint-disable no-underscore-dangle */
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw TypeError('Concersion Rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
