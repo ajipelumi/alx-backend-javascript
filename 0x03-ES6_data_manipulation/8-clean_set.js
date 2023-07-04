8f43c9d66bd8011e0ec0ceec9d9cd27046117570
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-04 19:35:51 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,14 +2,11 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  if (typeof startString !== 'string') {
-    return '';
-  }
 
   const newSet = [];
   set.forEach((item) => {
     if (item.startsWith(startString)) {
-      newSet.push(item.slice(startString.length));
+      newSet.push(item.substring(startString.length));
     }
   });
 
