dba1f018e0e50d63ed7b9049777ad9764f93efd5
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-06 18:57:46 +0100

    handled namespace and declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects.d.ts
@@ -0,0 +1,7 @@
+import { Subject } from './subjects';
+import { Teacher } from './subjects';
+import { Cpp } from './subjects';
+import { Java } from './subjects';
+import { React } from './subjects';
+
+export = Subjects;
